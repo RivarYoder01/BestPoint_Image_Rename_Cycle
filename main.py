acf7446b11f62094b712d5c6d9a4d561ff8c0ca2
RivarYoder01 <alyssa.stars.101@gmail.com>
2025-06-16 16:20:43 -0500

    testing

--- main.py
+++ main.py
@@ -10,21 +10,39 @@ __version__ = '1.0'
 __date__ = '2025.06.16'
 __status__ = 'Development'
 
-from Images import *
-import os
-from PIL import Image
+import os, os.path
+from datetime import datetime
+# import tkinter as tk
+# from tkinter import ttk, filedialog
+
+def rename_images():
+    directory = "C:\\Users\\alyss\\PycharmProjects\\BestPoint_Image_Rename_Cycle\\Images"
+    current_year = datetime.now().year
+
+    # Iterate over all files in the directory
+    for i, filename in enumerate(os.listdir(directory)):
+        # Check if the file is an image
+        try:
+            if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+                # Create the new file name
+                new_name = ("AHHHH" + f"_{i}.png")
+                # new_name = (str(current_year) + f"_{i}")
+
+                # Create the full file path
+                old_path = os.path.join(directory, filename)
+                new_path = os.path.join(directory, new_name)
+
+                # Rename the file
+                os.rename(old_path, new_path)
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        else:
+            print("Images Renamed :)")
 
 
 def main():
-    image_list = []
-    for filename in os.listdir(Images):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-            try:
-                img = Image.open(os.path.join(Images, filename))
-                image_list.append(img)
-            except Exception as e:
-                print(f"Error loading image {filename}: {e}")
-
+    rename_images()
 
 
 if __name__ == "__main__":

61bae658d0320614bc10bc0e05aa7b4d257adc2d
RivarYoder01 <alyssa.stars.101@gmail.com>
2025-06-16 13:36:16 -0500

    Main Creation

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+
+
+"""
+
+__author__ = 'Rivar Yoder'
+__version__ = '1.0'
+__date__ = '2025.06.16'
+__status__ = 'Development'
+
+
+def main():
+    print("Start")
+
+
+if __name__ == "__main__":
+    main()

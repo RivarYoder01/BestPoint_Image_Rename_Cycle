326bc076b305dba56a5975e1b32f41e02dd9a73c
Rivar <156348015+rivaryoder01@users.noreply.github.com>
2025-06-16 13:40:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ __version__ = '1.0'
 __date__ = '2025.06.16'
 __status__ = 'Development'
 
+import os
+
 
 def main():
-    print("Start")
+    img_dir = "BestPoint_Image_Rename_Cycle/Images"
+
+    image_list = []
+    for filename in os.listdir(img_dir):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_list.append(os.path.join(img_dir, filename))
+
+    print(image_list)
 
 
 if __name__ == "__main__":
